d6dbfa3a66e3d2a8ac1975c917488dbb39a73ab7
Konovalov.N <nikitakonovalov2013@yandex.ru>
2018-11-12 03:49:47 +0300

    Полностью корректно работает фунция генерации ключей

--- 3dcipher.c
+++ 3dcipher.c
@@ -9,13 +9,14 @@ typedef uint64_t u64;
 
 #define file_len(x) (unsigned long)x
 #define b_64(x) 0x0000000000000000 | x
+
 #define N 4
 
-static u8 MDS_4[16] = {0x0, 0x8, 0x0, 0x4, 0x0, 0x2, 0x0, 0x1, 0x9, 0x0, 0xb, 0x0, 0xd, 0xd, 0xe, 0xe};//Матрица MDS для n=4
+static u64 MDS_4 = {0x0804020190b0ddee};//Матрица MDS для n=4
 static u8 SbTab_4[16] = {0x7, 0xa, 0x2, 0xc, 0x4, 0x8, 0xf, 0x0, 0x5, 0x9, 0x1, 0xe, 0x3, 0xd, 0xb, 0x6};//Таблица S-блоков для n=4
 
 
-static u64 Key_Matrix(u64, u8[]);//Функция умножения на матрицу MDS 
+static u64 Key_Matrix(u64, u64);//Функция умножения на матрицу MDS 
 static u64 *KeyShedule(u64[]);//Генерация цикл. ключей
 static u8 SboxByte(u8);//Функция преобразования S-блока для одного байта
 static u8 InTab(u64);//Функция преобразования информационного блока в 3D матрицу
@@ -28,9 +29,31 @@ static u8 Perm(u8);//Применение перестановки к инфор
 static u64 Cipher(u64, u64);//Функция шифрования
 static u64 InvCipher(u64, u64);//Функция расшифрования
 
-static u64 Key_Matrix(u64 k, u8 mds[]) {
-	u64 x = 0xffffffffffffffff;
-	return x;
+static u64 Key_Matrix(u64 k, u64 MDS) {
+
+	u8 key[8][8];
+	u8 mds[8][8];
+	u8 result[8][8];
+	u64 retval = 0;
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			key[i][j] = (u8)(k>>63);
+			mds[i][j] = (u8)(MDS>>63);
+			k = k<<1;
+			MDS = MDS<<1;
+
+			result[i][j] = 0;
+		
+			for (int k = 0; k < 8; k++)
+				result[i][j] = result[i][j] + (key[i][k]*mds[k][j]);
+		
+			retval |= (u64)result[i][j];
+			retval <<= 1;
+		}
+	}	
+
+	return retval; 
 }
 
 static u64 *KeyShedule(u64 key[2]) {
@@ -39,12 +62,12 @@ static u64 *KeyShedule(u64 key[2]) {
 	keyRound[0] = key[1];
 	keyRound[1] = key[0]^b_64(1);
 	
-	for ( int i = 2; i <= 15; i++) {
+	for (int i = 2; i <= 15; i++) {
 		u64 K_A = Key_Matrix(keyRound[i-1], MDS_4); 
-		keyRound[i] = K_A ^ keyRound[i-2] ^ b_64(i); 
+		keyRound[i] =K_A^keyRound[i-2]^b_64(i);
+		printf("0x%016lx\n",keyRound[i]);	
 	}
 		
-	//printf("0x%lx\n",keyRound[5]);
 	return keyRound; 
 }
 
@@ -58,10 +81,10 @@ static u8 SboxByte(u8 x) {
 
 int main(int argc, char* argv[]) {
 
-	u64 key[2] = {0xfffffffffffffff0, 0x0000000000000000};
+	u64 key[2] = {0x0001020304050607, 0x08090a0b0c0d0e0f};
 	u64 *keytab = KeyShedule(key);
 	free(keytab);
-	
+
 	u8 *in[N][N][N];
 	u8 *out[N][N][N];
 
